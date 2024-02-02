c34fc192566dd20feaea1730e8e386d795c30ae2
vamshu12323 <vshlvani1234@gmail.com>
2024-02-02 21:54:14 +0530

    Till lec 4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
+let name = "Shreya";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+
+      <div className="container">
+        <h1>Hello {name}</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+          porro labore molestiae eligendi neque illo impedit sapiente in non?
+          Quaerat vel itaque voluptatem similique cupiditate ipsam reiciendis
+          culpa praesentium inventore accusamus, voluptate ad nisi.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
 }
 
