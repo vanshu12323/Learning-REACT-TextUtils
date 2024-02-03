da93da0a1c538bf672df434cd02ed554a98216de
vanshu12323 <vshlvani1234@gmail.com>
2024-02-03 17:58:40 +0530

    prop and prop types

---
+++ module1.mjs
@@ -0,0 +1,3 @@
+import ui, { a, c } from "./module2.mjs";
+console.log(ui);
+console.log(a);
---
+++ module2.mjs
@@ -0,0 +1,8 @@
+const a = "ABC";
+const b = "DEF";
+const c = "GHI";
+const d = "JKL";
+
+export default b;
+export { a };
+export { c };
